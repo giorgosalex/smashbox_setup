783dc160690253052f91def5367a820cad37ff69
Georgios Alexandropoulos <georgios.alexandropoulos@cern.ch>
2018-05-30 16:21:06 +0200

    add update_repo to update repository every day

---
+++ update_repo.py
@@ -0,0 +1,25 @@
+import sys, os, shutil
+
+def install_and_import(pkg):
+    import importlib, site
+    
+    os.system(sys.executable + " -m easy_install " + pkg)
+
+    reload(site)
+    importlib.import_module(pkg)
+    globals()[pkg] = importlib.import_module(pkg)
+
+
+
+install_and_import('wget')
+
+shutil.rmtree("C:\\smashbox-master")
+
+wget.download("https://github.com/cernbox/smashbox/archive/master.zip")
+    
+import zipfile
+with zipfile.ZipFile("smashbox-master.zip", 'r') as zip_ref:
+    zip_ref.extractall("C:\\")
+
+#shutil.move("smashbox-master.zip",'\\')
+os.remove("smashbox-master.zip")
